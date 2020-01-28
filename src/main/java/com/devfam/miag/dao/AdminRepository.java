fe154739c2778a8ce74fb375cbee3a2e14589d0e
Oumar Ba <bows4844@gmail.com>
2020-01-28 20:04:21 +0100

    0 changement

--- src/main/java/com/devfam/miag/dao/AdminRepository.java
+++ src/main/java/com/devfam/miag/dao/AdminRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devfam.miag.entities.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+	Admin findByNni(String nni);
 }
--- src/main/java/com/devfam/miag/services/CompteServiceImp.java
+++ src/main/java/com/devfam/miag/services/CompteServiceImp.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.devfam.miag.dao.CompteRepository;
 import com.devfam.miag.entities.Compte;
-<<<<<<< HEAD
-=======
+
 
 public class CompteServiceImp implements CompteService {
->>>>>>> e639bad3ba7673b0db71c72aa147678781d19918
+
 
 	// declaration de l'objet CompteRepository pour les traitement avec le DAO
 	@Autowired
@@ -17,7 +16,7 @@ public class CompteServiceImp implements CompteService {
 	@Override
 	public double checkSolde(String numCompte) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		Compte compte=compteRepo.findByNumCompte(numCompte);
 		
 		if(compte == null) {
@@ -25,27 +24,32 @@ public class CompteServiceImp implements CompteService {
 		}else {
 		
 			return compte.getSolde() ;}
-=======
-
-		return 0;
->>>>>>> e639bad3ba7673b0db71c72aa147678781d19918
 	}
 
+	/*.........................................................................................*/
+	/*.........................................................................................*/
 	@Override
 	public boolean sendMoney(String numCompteSource, String numCompteDest, double somme) {
-		// TODO Auto-generated method stub
-		Compte compte1 = compteRepo.findByNumCompte(numCompteSource);
-		Compte compte2 = compteRepo.findByNumCompte(numCompteDest);
-		double b;
-		double s = compte2.getSolde();
-		if(compte1.getSolde() != 0 && compte1.getSolde() > somme ) {
-			b = compte1.getSolde() - somme;
-		    s += b;
-			return true;
+		//Envoyer de l'aregent
+		
+		double balance = checkSolde(numCompteSource);
+		if(balance != -1 ) {
+			//verication si le compte existe
+			Compte compte1 = compteRepo.findByNumCompte(numCompteSource);
+			Compte compte2 = compteRepo.findByNumCompte(numCompteDest);
 			
-		}else return false;
+			if(compte1.getSolde() != 0 && compte1.getSolde() > somme ) {
+				//verification si le solde contient une valeur superieur à somme 
+				compte2.setSolde(compte1.getSolde() - somme);
+				compteRepo.save(compte2);
+				return true;
+			}
+			
+		}
+		return false;
 	}
-
+	/*.........................................................................................*/
+	/*.........................................................................................*/
 	@Override
 	public boolean crediteAccount(String numCompte, double somme) {
 		//Credit comppte code
