6b561817218fdfcfd9d87758b611341e149980de
Oumar Ba <bows4844@gmail.com>
2020-01-30 14:20:46 +0100

    Stated Service tests

--- src/main/java/com/devfam/miag/dao/ClientRepository.java
+++ src/main/java/com/devfam/miag/dao/ClientRepository.java
@@ -2,8 +2,10 @@ package com.devfam.miag.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.devfam.miag.entities.Client;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
 
+public interface ClientRepository extends JpaRepository<Client, Long> {
+	Client findByNni(String nni);
 }
--- src/main/java/com/devfam/miag/services/CompteServiceImp.java
+++ src/main/java/com/devfam/miag/services/CompteServiceImp.java
@@ -1,6 +1,7 @@
 package com.devfam.miag.services;
 
 import java.util.Date;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +9,14 @@ import org.springframework.stereotype.Service;
 
 import com.devfam.miag.dao.CompteRepository;
 import com.devfam.miag.entities.Compte;
-<<<<<<< HEAD
-
-=======
->>>>>>> 88c13543b3efa8e05fa6307cc92926b01fb10e8d
 
 
 @Service
 public class CompteServiceImp implements CompteService {
-<<<<<<< HEAD
 
-=======
->>>>>>> 88c13543b3efa8e05fa6307cc92926b01fb10e8d
+
+	/*.........................................................................................*/
+	/*.........................................................................................*/
 
 	// declaration de l'objet CompteRepository pour les traitement avec le DAO
 	@Autowired
@@ -28,32 +25,20 @@ public class CompteServiceImp implements CompteService {
 	@Override
 	public double checkSolde(String numCompte) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-
 		Compte compte=compteRepo.findByNumCompte(numCompte);
 		
 		if(compte == null) {
 			return -1;
 		}else {
 		
-			return compte.getSolde() ;}
-=======
-		Compte compte = compteRepo.findByNumCompte(numCompte);
-
-		if (compte == null) {
-			return -1;
-		} else {
-
-			return compte.getSolde();
-		}
->>>>>>> 88c13543b3efa8e05fa6307cc92926b01fb10e8d
+			return compte.getSolde() ;
+			}
 	}
 
 	/*.........................................................................................*/
 	/*.........................................................................................*/
 	@Override
 	public boolean sendMoney(String numCompteSource, String numCompteDest, double somme) {
-<<<<<<< HEAD
 		//Envoyer de l'aregent
 		
 		double balance = checkSolde(numCompteSource);
@@ -71,20 +56,7 @@ public class CompteServiceImp implements CompteService {
 			
 		}
 		return false;
-=======
-		// TODO Auto-generated method stub
-		Compte compte1 = compteRepo.findByNumCompte(numCompteSource);
-		Compte compte2 = compteRepo.findByNumCompte(numCompteDest);
-		double b;
-		double s = compte2.getSolde();
-		if (compte1.getSolde() != 0 && compte1.getSolde() > somme) {
-			b = compte1.getSolde() - somme;
-			s += b;
-			return true;
 
-		} else
-			return false;
->>>>>>> 88c13543b3efa8e05fa6307cc92926b01fb10e8d
 	}
 	/*.........................................................................................*/
 	/*.........................................................................................*/
